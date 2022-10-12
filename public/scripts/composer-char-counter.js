ffb5bb7a39b12cc8192c70d84cd406b3f4b11cd8
Matt Wilson <mwilson82@hotmail.com>
2022-10-12 00:29:40 +0000

    implemented character counter for tweeter text area and css for changing counter red when invalid

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  console.log("document is ready")
+});
+
+
+
+
+const $textArea = $('#tweet-text') 
+//Adjusts page counter based on amount of characters typed into textArea
+$textArea.on("input", function() {
+  const counter = $(this).closest("form").find("#counter")[0]
+  counter.value=(140 - $(this).val().length)
+  //Turns counter red if character limit is exceeded
+  if (counter.value <= -1) {
+    $(counter).addClass('active')
+  } else if (counter.value <= 0) {
+    $(counter).removeClass('active')
+  }
+})
